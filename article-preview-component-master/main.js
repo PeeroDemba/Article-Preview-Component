798ae737f53f466cd34ab6ab7e0b92a2af2ab839
Peter Jonathan-Hart <peerodemba@gmail.com>
2023-02-11 03:42:57 +0100

    first commit

---
+++ article-preview-component-master/main.js
@@ -0,0 +1,35 @@
+var icons = document.getElementById("icons");
+var share = document.getElementById("share");
+var close = document.getElementById("close");
+var pad = document.getElementById("change");
+
+function switcher1() {
+  if (icons.className == "hide") {
+    icons.removeAttribute("class", "hide");
+    icons.setAttribute("class", "show");
+    close.removeAttribute("class", "hide");
+    close.setAttribute("class", "show");
+    share.removeAttribute("class", "show");
+    share.setAttribute("class", "hide");
+    change.removeAttribute("class", "share");
+    change.setAttribute("class", "col");
+  } else {
+  }
+}
+
+function switcher2() {
+  if (icons.className == "show") {
+    icons.removeAttribute("class", "show");
+    icons.setAttribute("class", "hide");
+    close.removeAttribute("class", "show");
+    close.setAttribute("class", "hide");
+    share.removeAttribute("class", "hide");
+    share.setAttribute("class", "show");
+    change.removeAttribute("class", "col");
+    change.setAttribute("class", "share");
+  } else {
+  }
+}
+
+share.addEventListener("click", switcher1);
+close.addEventListener("click", switcher2);
